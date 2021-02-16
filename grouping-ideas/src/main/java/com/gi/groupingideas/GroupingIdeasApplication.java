b202723d171792db408906a7e31ab9e2a81e2921
Sethu Sugendran <ssugendran@altimetrik.com>
2021-02-16 20:48:20 +0530

    Initial commit

---
+++ grouping-ideas/src/main/java/com/gi/groupingideas/GroupingIdeasApplication.java
@@ -0,0 +1,13 @@
+package com.gi.groupingideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupingIdeasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupingIdeasApplication.class, args);
+	}
+
+}
---
+++ grouping-ideas/src/test/java/com/gi/groupingideas/GroupingIdeasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gi.groupingideas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupingIdeasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
